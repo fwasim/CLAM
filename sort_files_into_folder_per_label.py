48c597b1e6d47f7ede3639e34c3a23505ff2fce5
Farhan Wasim <fawasim@mist-login01.scinet.local>
2023-07-25 02:32:08 -0400

    Fix issues in script from last commit

--- sort_files_into_folder_per_label.py
+++ sort_files_into_folder_per_label.py
@@ -3,20 +3,20 @@ from os.path import isfile, join
 import pandas as pd
 import shutil
 
-PATH_TO_EXCEL_FILE = '/Users/farhanahmedwasim/Desktop/MEng/Final project/dataset_images_100_percent.xlsx'
-FEATURES_DIR = '/Users/farhanahmedwasim/Desktop/MEng/Final project/Image features'
+PATH_TO_EXCEL_FILE = '/scratch/d/dsussman/fawasim/dataset_images_100_percent.xlsx'
+FEATURES_DIR = '/scratch/d/dsussman/fawasim/Image_features'
 FEATURES_DIR_H5 = FEATURES_DIR + '/h5_files'
 FEATURES_DIR_PT = FEATURES_DIR + '/pt_files'
-DEST_DIR = ''
+DEST_DIR = '/scratch/d/dsussman/fawasim/Output'
 DEST_DIR_BENIGN = DEST_DIR + '/Benign'
-DEST_DIR_BENIGN_H5 = DEST_DIR_BENIGN + '/FEATURES_DIRECTORY_MATTHEW/h5_files'
-DEST_DIR_BENIGN_PT = DEST_DIR_BENIGN + '/FEATURES_DIRECTORY_MATTHEW/pt_files'
+DEST_DIR_BENIGN_H5 = DEST_DIR_BENIGN + '/FEATURES_DIRECTORY_MATTHEW/h5_files/'
+DEST_DIR_BENIGN_PT = DEST_DIR_BENIGN + '/FEATURES_DIRECTORY_MATTHEW/pt_files/'
 DEST_DIR_CANCER = DEST_DIR + '/Cancer'
-DEST_DIR_CANCER_H5 = DEST_DIR_CANCER + '/FEATURES_DIRECTORY_MATTHEW/h5_files'
-DEST_DIR_CANCER_PT = DEST_DIR_CANCER + '/FEATURES_DIRECTORY_MATTHEW/pt_files'
+DEST_DIR_CANCER_H5 = DEST_DIR_CANCER + '/FEATURES_DIRECTORY_MATTHEW/h5_files/'
+DEST_DIR_CANCER_PT = DEST_DIR_CANCER + '/FEATURES_DIRECTORY_MATTHEW/pt_files/'
 DEST_DIR_HYPERPLASIA = DEST_DIR + '/Hyperplasia'
-DEST_DIR_HYPERPLASIA_H5 = DEST_DIR_HYPERPLASIA + '/FEATURES_DIRECTORY_MATTHEW/h5_files'
-DEST_DIR_HYPERPLASIA_PT = DEST_DIR_HYPERPLASIA + '/FEATURES_DIRECTORY_MATTHEW/pt_files'
+DEST_DIR_HYPERPLASIA_H5 = DEST_DIR_HYPERPLASIA + '/FEATURES_DIRECTORY_MATTHEW/h5_files/'
+DEST_DIR_HYPERPLASIA_PT = DEST_DIR_HYPERPLASIA + '/FEATURES_DIRECTORY_MATTHEW/pt_files/'
 
 onlyfiles = [f for f in listdir(FEATURES_DIR_H5) if isfile(join(FEATURES_DIR_H5, f))]
 
@@ -28,7 +28,7 @@ print('\nHow many labels are there:\n')
 print(df['Label'].value_counts())
 ids = list(df['Participant ID'])
 
-excluded_df = None
+excluded_df = pd.DataFrame(columns=df.columns)
 
 for i in onlyfiles:
     file_name = int(i.split('.')[0])
@@ -38,28 +38,31 @@ for i in onlyfiles:
         label = df["Label"].to_string()
 
         if label == 'Benign':
-            shutil.copyfile(FEATURES_DIR_H5 + '/' + i, DEST_DIR_BENIGN_H5)
-            shutil.copyfile(FEATURES_DIR_PT + '/' + i, DEST_DIR_BENIGN_PT)
+            print("Copying file " + FEATURES_DIR_H5 + '/' + i + " to " + DEST_DIR_BENIGN_H5 + i)
+            shutil.copyfile(FEATURES_DIR_H5 + '/' + i, DEST_DIR_BENIGN_H5 + i)
+            shutil.copyfile(FEATURES_DIR_PT + '/' + str(file_name) + '.pt', DEST_DIR_BENIGN_PT + str(file_name) + '.pt')
         elif label == 'Neoplasia':
-            shutil.copyfile(FEATURES_DIR_H5 + '/' + i, DEST_DIR_CANCER_H5)
-            shutil.copyfile(FEATURES_DIR_PT + '/' + i, DEST_DIR_CANCER_PT)
+            print("Copying file " + FEATURES_DIR_H5 + '/' + i + " to " + DEST_DIR_CANCER_H5 + i)
+            shutil.copyfile(FEATURES_DIR_H5 + '/' + i, DEST_DIR_CANCER_H5 + i)
+            shutil.copyfile(FEATURES_DIR_PT + '/' + str(file_name) + '.pt', DEST_DIR_CANCER_PT + str(file_name) + '.pt')
         else:
-            shutil.copyfile(FEATURES_DIR_H5 + '/' + i, DEST_DIR_HYPERPLASIA_H5)
-            shutil.copyfile(FEATURES_DIR_PT + '/' + i, DEST_DIR_HYPERPLASIA_PT)
+            print("Copying file " + FEATURES_DIR_H5 + '/' + i + " to " + DEST_DIR_HYPERPLASIA_H5 + i)
+            shutil.copyfile(FEATURES_DIR_H5 + '/' + i, DEST_DIR_HYPERPLASIA_H5 + i)
+            shutil.copyfile(FEATURES_DIR_PT + '/' + str(file_name) + '.pt', DEST_DIR_HYPERPLASIA_PT + str(file_name) + '.pt')
     else:
-        if excluded_df == None:
+        if excluded_df.empty:
             excluded_df = df
         else:
             excluded_df.append(df)
 
 print("\nNo. of Benign files copied:")
-print("h5 files:\t" + len([name for name in listdir('.') if isfile(join(DEST_DIR_BENIGN_H5, name))]))
-print("pt files:\t" + len([name for name in listdir('.') if isfile(join(DEST_DIR_BENIGN_PT, name))]))
+print("h5 files:\t" + str(len([name for name in listdir('.') if isfile(join(DEST_DIR_BENIGN_H5, name))])))
+print("pt files:\t" + str(len([name for name in listdir('.') if isfile(join(DEST_DIR_BENIGN_PT, name))])))
 
 print("\nNo. of Cancer files copied:")
-print("h5 files:\t" + len([name for name in listdir('.') if isfile(join(DEST_DIR_CANCER_H5, name))]))
-print("pt files:\t" + len([name for name in listdir('.') if isfile(join(DEST_DIR_CANCER_PT, name))]))
+print("h5 files:\t" + str(len([name for name in listdir('.') if isfile(join(DEST_DIR_CANCER_H5, name))])))
+print("pt files:\t" + str(len([name for name in listdir('.') if isfile(join(DEST_DIR_CANCER_PT, name))])))
 
 print("\nNo. of Hyperplasia files copied:")
-print("h5 files:\t" + len([name for name in listdir('.') if isfile(join(DEST_DIR_HYPERPLASIA_H5, name))]))
-print("pt files:\t" + len([name for name in listdir('.') if isfile(join(DEST_DIR_HYPERPLASIA_PT, name))]))
+print("h5 files:\t" + str(len([name for name in listdir('.') if isfile(join(DEST_DIR_HYPERPLASIA_H5, name))])))
+print("pt files:\t" + str(len([name for name in listdir('.') if isfile(join(DEST_DIR_HYPERPLASIA_PT, name))])))
